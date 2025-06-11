d4e6a3ebd9fd7a631469e25f26feb9a610d0dce5
martinxjonsson <jonssonxmartin@gmail.com>
2025-06-11 13:38:02 +0200

    DE-2025-06-11 Afternoon

---
+++ DE-2025-06-11/API.py
@@ -0,0 +1,10 @@
+import requests
+
+kattig_fakta = []
+for _ in range(5):
+    response = requests.get("https://catfact.ninja/fact")
+    data = response.json()
+    kattig_fakta.append(data["fact"])
+
+for fact in kattig_fakta:
+    print(fact)
--- DE-2025-06-11/jsontingz.py
+++ DE-2025-06-11/jsontingz.py
@@ -2,12 +2,23 @@
 
 import json
 
-person = {
-    "name": "Martin Jonsson",
-    "age": 34,
-    "skills": ["Python", "SQL", "Data"],
+item = {
+    "name": "Book of bro knowledge",
+    "rarity": "Tier 10 type shit",
+    "durability": 100,
+    "bookmark-page": 55,
  }
 
-
-json_string = json.dumps(person, indent=2)
+json_string = json.dumps(item, indent=2)
 print(json_string)
+
+with open("item.json", "w") as file:
+    json.dump(item, file, indent=2)
+
+with open("item.json", "r") as file:
+    loaded_item = json.load(file)
+
+print("Name:", loaded_item["name"])
+print("Bookmark-page:", loaded_item["bookmark-page"])
+
+    
