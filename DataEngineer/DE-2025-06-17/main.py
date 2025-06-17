14eb7f0c0c8deb218e7a553b16c7627b31540a5c
martinxjonsson <jonssonxmartin@gmail.com>
2025-06-17 13:04:46 +0200

    Updates

---
+++ DataEngineer/DE-2025-06-17/main.py
@@ -0,0 +1,11 @@
+from src.data.queries import connect
+from src.data.queries import get_all_person
+from src.data.queries import get_column_name
+
+if __name__ == "__main__":
+    print("All info")
+    get_all_person()
+
+if __name__ == "__main__":
+    print("Kolumner")
+    get_column_name()
